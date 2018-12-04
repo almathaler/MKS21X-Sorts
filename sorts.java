7e7817646388d698634b925931c93b28372690f2
almathaler <athaler00@stuy.edu>
2018-12-04 18:53:39 -0500

    works with short lists of integers

---
+++ sorts.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+public class sorts{
+  public static void main(String[] args){
+    int[] arguements = new int[args.length];
+    for (int i = 0; i<args.length; i++){
+      arguements[i] = Integer.parseInt(args[i]);
+    }
+    System.out.println("These are your args: " + Arrays.toString(arguements));
+    //swap(arguements, 2, 4);
+    //System.out.println("Swap 4th and 2nd index: " + Arrays.toString(arguements));
+    selectionsort(arguements);
+    System.out.println("selectionsorted args: " + Arrays.toString(arguements));
+    //selectionsort(arguements);
+    //System.out.println("Sorted with selectionsort: " + Arrays.toString(arguements));
+  }
+  public static void swap(int[] ary, int i1, int i2){
+    int hold = ary[i2];
+    ary[i2] = ary[i1];
+    ary[i1] = hold;
+  }
+
+  public static void selectionsort(int[] ary){
+    for(int currentIndex = 0; currentIndex < ary.length; currentIndex++){
+      int indexOfSmall = currentIndex;
+      for (int i = currentIndex; i<ary.length; i++){
+        if (ary[i] < ary[indexOfSmall]){
+          indexOfSmall = i;
+        }
+      }
+      System.out.println("CurrentIndex: " + currentIndex + " indexOfSmall: " + indexOfSmall + " value of small: " + ary[indexOfSmall]);
+      swap(ary, indexOfSmall, currentIndex);
+    }
+  }
+}
