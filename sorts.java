c5a2e72313cabae229bb81f77c682eab03889c0f
almathaler <athaler00@stuy.edu>
2018-12-10 18:16:06 -0500

    got insertionSort to work

--- sorts.java
+++ sorts.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 public class Sorts{
-/**
+
   public static void main(String[] args){
     int[] arguements = new int[args.length];
     for (int i = 0; i<args.length; i++){
@@ -9,18 +9,17 @@ public class Sorts{
     System.out.println("These are your args: " + Arrays.toString(arguements));
     //swap(arguements, 2, 4);
     //System.out.println("Swap 4th and 2nd index: " + Arrays.toString(arguements));
-    bubbleSort(arguements);
-    System.out.println("bubbleSorted args: " + Arrays.toString(arguements));
+    insertionSort(arguements);
+    System.out.println("insertionSorted args: " + Arrays.toString(arguements));
     //selectionsort(arguements);
     //System.out.println("Sorted with selectionsort: " + Arrays.toString(arguements));
   }
-**/
   public static void swap(int[] ary, int i1, int i2){
     int hold = ary[i2];
     ary[i2] = ary[i1];
     ary[i1] = hold;
   }
-  /**Selection sort of an int array. 
+  /**Selection sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
@@ -32,13 +31,13 @@ public class Sorts{
           indexOfSmall = i;
         }
       }
-     //System.out.println("What array looks like now:" + Arrays.toString(ary)); 
+     //System.out.println("What array looks like now:" + Arrays.toString(ary));
      //System.out.println("CurrentIndex: " + currentIndex + " indexOfSmall: " + indexOfSmall + " value of small: " + ary[indexOfSmall]);
 //      System.out.println("What array looks like now:" + Arrays.toString(ary));
       swap(ary, indexOfSmall, currentIndex);
     }
   }
- /**Bubble sort of an int array. 
+ /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
@@ -49,24 +48,42 @@ public class Sorts{
 		for (int i = 0; i<ary.length-1; i++){
 			if (ary[i]>ary[i+1]){
 				swap(ary,i,i+1);
-				aSort = true;			
+				aSort = true;
 			}
 		}
 		if (!aSort) {
 			done = true;
 		}
-		//System.out.println("What array looks like now:" + Arrays.toString(ary)); 	
+		//System.out.println("What array looks like now:" + Arrays.toString(ary));
 	}
  }
+
+ public static void insertionSort(int [] ary){
+   for (int i = 1; i<ary.length; i++){
+     System.out.println("Current array:" + Arrays.toString(ary));
+     int current = ary[i];
+     boolean sorted = false;
+     int j = 0;
+     if (current>ary[i-1]){
+       sorted = true;
+     }
+     while (!sorted){
+       if (current<ary[j]){
+         for (int k = i; k >= j; k--){
+           if (k == j){
+             ary[j] = current;
+           }
+           else{
+             ary[k] = ary[k-1];
+           }
+         }
+         //ary[j] = current;
+         sorted = true;
+       }
+       else{
+         j++;
+       }
+     }
+   }
+ }
 }
-
-
-
-
-
-
-
-
-
-
-
